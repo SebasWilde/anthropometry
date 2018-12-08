a2aa0be06bdb5e3315ae540a22cb737168a060a1
SebasWilde <sebaswilde@gmail.com>
2018-12-08 13:30:59 -0500

    build deportista model

--- root/models.py
+++ root/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 
 class Deportista(models.Model):
-    pass
+    nombres = models.CharField(max_length=45)
+    apellidos = models.CharField(max_length=45)
+    fecha_nacimiento = models.DateField()
+    foto = models.ImageField(upload_to='deportista', default='../media/defautl/default_user.png')
+
+    def __str__(self):
+        return self.nombres + self.apellidos
