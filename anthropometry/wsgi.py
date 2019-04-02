d6adfb1e5ada593d2466ca607639d6821890e4fa
SebasWilde <sebaswilde@gmail.com>
2019-04-02 16:50:44 -0500

    Fix main app

--- anthropometry/wsgi.py
+++ anthropometry/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for antropometry project.
+WSGI config for anthropometry project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'antropometry.settings.production')
+                      'anthropometry.settings.production')
 
 application = get_wsgi_application()
