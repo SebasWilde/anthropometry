09b9799086dafa16a7a5b65e8f932892e1018bf0
SebasWilde <sebaswilde@gmail.com>
2019-04-02 16:39:44 -0500

    Fixes

--- antropometry/settings/base.py
+++ antropometry/settings/base.py
@@ -105,6 +105,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, '../static_root/')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "../static"),
     'static',
