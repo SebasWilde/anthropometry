73e9d1222fc6bbeb23107db790c85324de178f6d
SebasWilde <sebaswilde@gmail.com>
2019-04-02 16:58:21 -0500

    Change settings

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn anthropometry.wsgi --log-file -
+web: gunicorn anthropometry.wsgi
--- anthropometry/settings/base.py
+++ anthropometry/settings/base.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = 'anthropometry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': [os.path.join(BASE_DIR, '../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -91,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
